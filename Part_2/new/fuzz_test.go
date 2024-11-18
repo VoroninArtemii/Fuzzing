eebec342fe5d0ca61ce2911a7b545bca30dabe71
Voronin Artemii <Voronin.Artemii.2003@gmail.com>
2024-11-18 18:07:14 +0300

    Upgrade fuzz-function

--- Part_2/new/fuzz_test.go
+++ Part_2/new/fuzz_test.go
@@ -1,10 +1,36 @@
-package fuzz
+package new
 
 import (
-	"testing"
+        "testing"
+        "k8s.io/apimachinery/pkg/runtime"
+        "k8s.io/apimachinery/pkg/runtime/schema"
+        "k8s.io/apimachinery/pkg/runtime/serializer/protobuf"
 )
 
-func FuzzParseImageName(f *testing.F) {
-	f.Add("url:5000/repo@sha256:e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855")
-	f.Fuzz(func(t *testing.T, input string) {})
+type SimpleObject struct {
+        Field1 string
+        Field2 int32
+}
+
+func (o *SimpleObject) GetObjectKind() schema.ObjectKind {
+        return &runtime.Unknown{}
+}
+
+func (o *SimpleObject) DeepCopyObject() runtime.Object {
+        return &SimpleObject{
+                Field1: o.Field1,
+                Field2: o.Field2,
+        }
+}
+
+func FuzzDecode(f *testing.F) {
+        serializer := protobuf.NewSerializer(nil, nil)
+        into := &SimpleObject{}
+        f.Fuzz(func(t *testing.T, originalData []byte) {
+                var gvk schema.GroupVersionKind
+                _, _, err := serializer.Decode(originalData, &gvk, into)
+                if err != nil && !protobuf.IsNotMarshalable(err) {
+                        t.Errorf("Decode failed with error: %v", err)
+                }
+        })
 }
